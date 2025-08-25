8cc06c13038c957f544a370f9dada6271425853f
Alex George <alexandre.george@beslogic.com>
2025-08-25 11:28:27 -0400

    make health not protected for deployment

--- app/api/endpoints/app_health_endpoint.py
+++ app/api/endpoints/app_health_endpoint.py
@@ -1,9 +1,7 @@
 from fastapi.security import HTTPBearer
 
-from fastapi import APIRouter, Depends
-from app.dependencies import get_current_user
+from fastapi import APIRouter
 from app.models.schemas.health_model import HealthModel
-from app.models.schemas.user import UserLogin
 
 security = HTTPBearer()
 
@@ -11,8 +9,6 @@ router = APIRouter(prefix="/health", tags=["health"])
 
 
 @router.get("", response_model=HealthModel)
-async def health_check(
-    _current_user: UserLogin = Depends(get_current_user),
-) -> HealthModel:
+async def health_check() -> HealthModel:
     response = HealthModel()
     return response
